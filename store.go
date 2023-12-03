594313580f88cd8c884f323cc40fe0e9fe081a73
Yuriy Kis <yurakis9@gmail.com>
2023-12-03 20:10:51 +0100

    Refactor file naming in store.go
    Signed-off-by: Yuriy Kis <yurakis9@gmail.com>

--- store.go
+++ store.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"crypto/md5"
 	"crypto/sha1"
 	"encoding/hex"
 	"io"
@@ -52,7 +54,11 @@ func (s *Store) writeStream(key string, r io.Reader) error {
 		return err
 	}
 
-	filename := "simefilename"
+	buf := new(bytes.Buffer)
+	io.Copy(buf, r)
+
+	filenameBytes := md5.Sum(buf.Bytes())
+	filename := hex.EncodeToString(filenameBytes[:])
 
 	pathAndFilename := pathName + "/" + filename
 
