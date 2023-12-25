8542296afefaa20a877e45975c4907e109b3eaae
Yuriy Kis <yurakis9@gmail.com>
2023-12-25 14:26:15 +0100

    Add comment to RPC struct describing its purpose
    Signed-off-by: Yuriy Kis <yurakis9@gmail.com>

--- p2p/message.go
+++ p2p/message.go
@@ -3,6 +3,7 @@ package p2p
 import "net"
 
 // RPC represents any arbitrary data that can be sent over the each transport between two nodes
+// can be a message, a file, a request, a response, etc.
 type RPC struct {
 	From    net.Addr
 	Payload []byte
