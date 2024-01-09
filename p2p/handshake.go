85c2a58a61c8f5cfad3df787cc7e89f9a42704b7
Yuriy Kis <yurakis9@gmail.com>
2024-01-09 18:52:30 +0100

    Add NOPHandshakeFunc as a no-op handshake function
    Signed-off-by: Yuriy Kis <yurakis9@gmail.com>

--- p2p/handshake.go
+++ p2p/handshake.go
@@ -3,4 +3,5 @@ package p2p
 // HandshakeFunc is a function that is called when a new connection is established.
 type HandshakeFunc func(Peer) error
 
+// NOPHandshakeFunc is a no-op handshake function.
 func NOPHandshakeFunc(Peer) error { return nil }
